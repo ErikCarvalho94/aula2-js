61f56a790a80cae5cc19befd27c90b3d8df25f42
Erik Carvalho <erik.carvalho94@hotmail.com>
2024-02-09 19:57:15 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt('Digite seu nome')
+alert('Bem-vindo ' + nome + ', vamos somar 2 números')
+
+let num1 = parseInt(prompt('Digite o primeiro número'))
+let num2 = parseInt(prompt('Digite o segundo número'))
+
+let valor = num1 + num2
+
+alert('O valor da soma é = ' + valor)
